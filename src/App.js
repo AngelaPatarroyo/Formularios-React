939fc943cdf3ebf5764f870ff7f66505ca9c561b
melbynrojo <101743782+melbynrojo@users.noreply.github.com>
2023-02-16 23:14:38 -0500

    Formularios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+    const [value, setValue] = useState({
+        normal: '',
+        texto: '',
+        select: '',
+        check: false,
+        estado: 'feliz'
+    })
+    const handleChange = ({ target }) => {
+        setValue((state) => ({
+            ...state,
+            [target.name]: target.type === 'checkbox'
+                ? target.checked
+                : target.value
+        })
+            /* ...value,
+            [e.target.name]: e.target.value }) */
+        )
+    }
+    console.log(value)
+    return (
+        <div>
+            {value.length < 5 ? <span>Longitud Minima de 5</span> : null}
+            <input name='normal' type='text' value={value.normal} onChange={handleChange} />
+            <textarea name='texto' onChange={handleChange} value={value.texto} />
+            <select value={value.select} name='select' onChange={handleChange}>
+                <option value=''>-- Seleccione --</option>
+                <option value='chanchofeliz'>Chancho Feliz</option>
+                <option value='chanchitofeliz'>Chanchito Feliz</option>
+                <option value='chanchitotriste'>Chanchito Triste</option>
+                <option value='felipe'>Felipe</option>
+            </select>
+            <input
+                type='checkbox'
+                name='check'
+                onChange={handleChange}
+                checked={value.check}
+            />
+            <div onChange={handleChange}>
+                <label>Chancho</label>
+                <input
+                    onChange={handleChange}
+                    type='radio'
+                    value='feliz'
+                    name='estado'
+                    checked={value.estado === 'feliz'}
+                /> Feliz
+                <input
+                    onChange={handleChange}
+                    type='radio' value='triste'
+                    name='estado'
+                    checked={value.estado === 'triste'}
+                />Triste
+                <input
+                    onChange={handleChange}
+                    type='radio'
+                    value='felipe'
+                    name='estado'
+                    checked={value.estado === 'felipe'}
+                />Felipe
+            </div>
+        </div>
+    )
+
 }
 
-export default App;
+export default App
